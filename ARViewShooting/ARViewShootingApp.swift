c3c935a7bbfda559fec432e2c0df4684ff4cea4f
Katsuhiro Masaki <hiro@digitaljet.co.jp>
2023-10-24 16:22:58 +0900

    Initial Commit

---
+++ ARViewShooting/ARViewShootingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARViewShootingApp.swift
+//  ARViewShooting
+//
+//  Created by Katsuhiro Masaki on 2023/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ARViewShootingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ARViewShooting/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ARViewShooting
+//
+//  Created by Katsuhiro Masaki on 2023/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
